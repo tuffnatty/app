61ae042cf4251f8d42923e3b245531bbdffce580
Garth Webb <garth@wikia-inc.com>
2012-11-02 21:14:30 +0000

    Revert this chang.  This belongs in dev

--- includes/wikia/wgCacheBuster.php
+++ includes/wikia/wgCacheBuster.php
@@ -4,9 +4,4 @@
  * various code-paths, some of which don't load the rest of the MediaWiki stack.
  */
 
-global $wgMedusaSlot;
-
-$slot_name = 'code' . ($wgMedusaSlot == 1 ? '' : $wgMedusaSlot);
-$cbFilePath = "/usr/wikia/deploy/$slot_name/src/wgCacheBuster.php";
-
-require_once($cbFilePath);
+$wgCacheBuster = '61849';
