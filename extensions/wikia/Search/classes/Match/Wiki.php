f6763f24963d0eac82dc21da4363cabf2a1e9de0
Robert Elwell <robert@wikia-inc.com>
2013-02-26 21:19:08 +0000

    ported over some mmore changes from refactor

--- extensions/wikia/Search/classes/Match/Wiki.php
+++ extensions/wikia/Search/classes/Match/Wiki.php
@@ -3,7 +3,7 @@
  * Class definition for \Wikia\Search\Match\Wiki
  */
 namespace Wikia\Search\Match;
-use \WikiaSearchResult as Result;
+use \Wikia\Search\Result as Result;
 use \WikiaHomePageHelper as HomePageHelper;
 
 class Wiki extends AbstractMatch
@@ -34,6 +34,7 @@ class Wiki extends AbstractMatch
 			$fields[\WikiaSearch::field( 'title' )] = $data['name'];
 			$fields['url'] = sprintf('%s://%s%s', $parsed['scheme'], $parsed['host'], $parsed['path']);
 			$fields['isWikiMatch'] = true;
+			$fields['thumbnail'] = array_shift( empty( $data['wiki_images'] ) ? array() : $data['wiki_images'] );
 
 			$result = new Result( $fields );
 			
@@ -59,6 +60,7 @@ class Wiki extends AbstractMatch
 				$fields['url'] = $this->interface->getMainPageUrlForWikiId( $this->id )
 				);
 		$result = new Result( $fields );
+		
 		
 		
 		$result->setText( $this->preprocessText( $this->getMainPageTextForWikiId( $this->id ) ) );
