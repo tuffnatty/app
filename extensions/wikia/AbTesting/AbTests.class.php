8bd121d5254482a4f9b992cc43cb682e61946c4e
Piotr Bablok <piotr.bablok@gmail.com>
2013-05-13 12:43:10 -0700

    API draft

---
+++ extensions/wikia/AbTesting/AbTests.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author: pbablok@wikia-inc.com
+ */
+
+
+class AbTests {
+
+	/*
+	 * return array of experiments currently running
+	 * experiment id => assigned group
+	 */
+	static function getExperiments() {
+
+	}
+
+	/*
+	 * for an experiment $exp return the *current* group that the user belongs to
+	 * returns null if user is not part of any groups for a given experiment
+	 */
+	static public function getGroup( $exp ) {
+
+	}
+
+
+}
